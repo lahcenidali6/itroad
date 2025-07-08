e21f235d1e546665494e6585ace01aeb30af648f
Lahcen Idali <lehcenidali6@gmail.com>
2025-07-08 14:45:52 +0100

    handle env file

--- server/config/db.js
+++ server/config/db.js
@@ -3,12 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const db = knex({
   client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  }
+  connection: process.env.DATABASE_URL,
 });
 console.log( process.env.DB_HOST)
 export default db;
